de9926eb6dc4eb5fa4d1d297f453bebc5012ea6a
appukuttan66 <72905732+appukuttan66@users.noreply.github.com>
2021-08-03 09:50:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -495,7 +495,7 @@ function followToggle(ele) {
         {
            follower: username,
            following: account,
-           what: ['blog'], //null value for unfollow, 'blog' for follow
+           what: ['blog'],
         },
       ]);
 
@@ -565,45 +565,23 @@ function getProfileInfo(u) {
   })
 }
 
+const notifywrkr = work("notify.js");
+
+notifywrkr.addEventListener("message",function(e){
+  document.getElementById("notify-body").innerHTML += e.data;
+});
+
 getNotifications();
-function getNotifications () {
-  var url = rpc;
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", url);
-
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  xhr.onreadystatechange = function () {
-     if (xhr.readyState === 4) {
-       console.log(xhr.status);
-       var json = JSON.parse(xhr.responseText);
-       var counter = 0;
-       var eleNot = document.getElementById("notify-body");
-       eleNot.innerHTML = ""
-       while (counter < json.result.length) {
-         var not = json.result[counter];
-         if(not === undefined) {
-           console.log(not)
-         }else {
-           let x;
-           if ( not.type == "reply" ) {
-            x = "&reply";
-           } else {
-            x = "";
-           }
-           eleNot.innerHTML += '<a class="nav-link p-0" href="/'+not.url.replace("@","?u=").replace("/","&p=")+x+'"><div class="alert rounded-0 border-0 alert-light my-0 d-flex align-item-center d-flex"><img src="https://images.ecency.com/u/'+not.msg.split(" ")[0].replace("@","")+'/avatar/small" width="36" height="36" class="rounded-circle flex-shrink-0 me-2" viewBox="0 0 16 16"><div>'+not.msg+'</div></div></a>';
-         }
-         
-       counter = counter + 1;
-       }
-     }
- }; 
-  var data = '{"jsonrpc":"2.0", "method":"bridge.account_notifications", "params":{"account": "'+username+'","limit":10}, "id":1}';
-
-  xhr.send(data);
-  
+function getNotifications() {
+  document.getElementById("notify-body").innerHTML = "";
+  notifywrkr.postMessage([rpc,username])
 }
+
+
+function work(me) {
+  var wrkr = new Worker(me);
+  return wrkr
+} 
 
 document.querySelectorAll('a[href="#discover"]').forEach(function(ele){
   ele.addEventListener('show.bs.tab', function (event) {
