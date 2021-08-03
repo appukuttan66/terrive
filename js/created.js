635ffe23bdd860407cb2f8a2b5603337b9f83b87
appukuttan66 <72905732+appukuttan66@users.noreply.github.com>
2021-08-03 12:55:08 +0530

    Delete created.js

--- js/created.js
+++
@@ -1,8 +0,0 @@
-addEventListener("message", event => {
-  const ha = event.data
-  ha.getDiscussionsByCreated({tag: 'trhome', limit: 35, truncate_body: 1,} , function(err,res) {
-    if ( err === null ) {
-      postMessage(res);
-    } else {console.log(err);}
-  });
-})
