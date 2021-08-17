b65ea876f198085523f8436e3278ec0f750ea56b
appukuttan66 <72905732+appukuttan66@users.noreply.github.com>
2021-08-17 22:15:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,37 @@
-const cacheNAME = "tr-cache-v2";
-const cacheURLS = [
-  'fonts/satisfy-v11-latin-regular.woff2',
-  'js/b58.js',
-  'css/satisfy.css'
-];
+const CACHE_NAME = "tr-offline";
+const CACHE_URL = "offline.html"
 
 self.addEventListener("install",function(installEvent){
   installEvent.waitUntil(
-    caches.open(cacheNAME).then(function(cache){
-      return cache.addAll(cacheURLS);
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.add(new Request(CACHE_URL, { cache: "reload" });
     })
   );
+  self.skipWaiting()
 });
+
+self.addEventListener("activate",function(e){
+  e.waitUntil(
+    if ("navigationPreload" in self.registration) {
+      self.registration.navigationPreload.enable();
+    }
+  )
+  self.clients.claim()
+})
 
 self.addEventListener("fetch",function(e){
   e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request);
-    })
+    try {
+      if(e.preloadResponse) {
+        return e.preloadResponse
+      }
+      return fetch(e.request)
+    }
+    catch (err) {
+      console.log(err)
+      caches.open(CACHE_NAME).then(function(cache){
+        return cache.match(CACHE_URL)
+      })
+    }
   );
 });
