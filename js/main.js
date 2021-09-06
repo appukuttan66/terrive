0819242dbcba6c8180233b869458065f945cecac
appukuttan66 <72905732+appukuttan66@users.noreply.github.com>
2021-09-06 17:04:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -395,10 +395,12 @@ function editPost() {
   
   hive.api.getContent(pa,pp,function(e,r){
     if (e === null) {
-    r.json_metadata.description = body.value;
+    let json = JSON.parse(r.json_metadata);
+    
+      json.description = body.value;
       
       if(accessToken) {
-        client.comment('',r.category,r.author,r.permlink,r.title,body.value,r.json_metadata,function(err,res){
+        client.comment('',r.category,r.author,r.permlink,r.title,body.value,json,function(err,res){
           if (err === null) {
             editele.hide();
             notify("Successfully edited !")
@@ -408,7 +410,7 @@ function editPost() {
           }
         })
       }else if (loginType = "keychain"){
-        hive_keychain.requestPost( r.author, r.title, body.value, r.category, '', r.json_metadata, r.permlink, '', function(rs){
+        hive_keychain.requestPost( r.author, r.title, body.value, r.category, '', json, r.permlink, '', function(rs){
           if ( rs.success == true ) {
             editele.hide("Successfully edited !");
             notify("Successfully edited !")
