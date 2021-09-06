1aafbbe9cb1f835b522e625fc190aaf668073a42
appukuttan66 <72905732+appukuttan66@users.noreply.github.com>
2021-09-06 17:56:41 +0530

    Update notify.js

--- js/notify.js
+++ js/notify.js
@@ -1,7 +1,41 @@
 addEventListener("message", event => {
   const [url, username] = event.data;
   var counter = 0;
+  var lastRead = "";
   
+  bridge(username,url,"unread_notificatins",function(r) {
+    const lastRead = new Date(JSON.parse(r).result.lastread.replace(" ","T"))
+  })
+  
+  bridge(username,url,"account_notifications",function(res){
+   
+    var json = JSON.parse(res);
+    while (counter < json.result.length) {
+    var not = json.result[counter];
+    if(not !== undefined) {
+       let x, alert;
+       if ( not.type == "reply" || not.type = "reply_comment" ) {
+        x = "&reply";
+       } else {
+        x = "";
+       }
+      var date = new Date(not.date)
+      
+      if(date > lastRead) {
+        alert = "alert-primary";
+      } else {
+        alert = "alert-light";
+      }
+        
+        var notifymsg = '<a class="nav-link p-0" href="/'+not.url.replace("@","?u=").replace("/","&p=")+x+'"><div class="alert rounded-0 border-0 '+alert+' my-0 d-flex align-item-center d-flex"><img src="https://images.ecency.com/u/'+not.msg.split(" ")[0].replace("@","")+'/avatar/small" width="36" height="36" class="rounded-circle flex-shrink-0 me-2" viewBox="0 0 16 16"><div>'+not.msg+'</div></div></a>';
+        postMessage(notifymsg)
+       }   
+      counter = counter + 1;
+    }
+  })
+});
+
+function bridge(username,url,type,callback) {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", url);
 
@@ -9,26 +43,10 @@ addEventListener("message", event => {
 
   xhr.onreadystatechange = function () {
      if (xhr.readyState === 4) {
-       console.log(xhr.status);
-       var json = JSON.parse(xhr.responseText);
-       while (counter < json.result.length) {
-         var not = json.result[counter];
-         if(not !== undefined) {
-           let x;
-           if ( not.type == "reply" ) {
-            x = "&reply";
-           } else {
-            x = "";
-           }
-           var notifymsg = '<a class="nav-link p-0" href="/'+not.url.replace("@","?u=").replace("/","&p=")+x+'"><div class="alert rounded-0 border-0 alert-light my-0 d-flex align-item-center d-flex"><img src="https://images.ecency.com/u/'+not.msg.split(" ")[0].replace("@","")+'/avatar/small" width="36" height="36" class="rounded-circle flex-shrink-0 me-2" viewBox="0 0 16 16"><div>'+not.msg+'</div></div></a>';
-           postMessage(notifymsg)
-         }
-         
-       counter = counter + 1;
-       }
+       callback(xhr.responseText)
      }
  }; 
-  var data = '{"jsonrpc":"2.0", "method":"bridge.account_notifications", "params":{"account": "'+username+'","limit":20}, "id":1}';
+  var data = '{"jsonrpc":"2.0", "method":"bridge.'+type+'", "params":{"account": "'+username+'","limit":20}, "id":1}';
 
   xhr.send(data);
-});
+}
